a4995629e1508c9e5af8723523d0fd1889023888
pli <b_igboss@163.com>
2022-06-02 21:47:35 +0800

    Symbolic links

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -291,7 +291,8 @@ sys_open(void)
   struct file *f;
   struct inode *ip;
   int n;
-
+  char p[MAXPATH];
+  int count=0;
   if((n = argstr(0, path, MAXPATH)) < 0 || argint(1, &omode) < 0)
     return -1;
 
@@ -320,6 +321,22 @@ sys_open(void)
     iunlockput(ip);
     end_op();
     return -1;
+  }
+
+  while (!(omode & O_NOFOLLOW) && ip->type == T_SYMLINK){
+    iunlockput(ip);
+    if(readi(ip, 0, (uint64) &p, 0, MAXPATH) != MAXPATH)
+      panic("symlink: readi");
+    if((ip = namei(p)) == 0){
+      end_op();
+      return -1;
+    }
+    ilock(ip);
+    if (count++ >= 10) {
+      iunlockput(ip);
+      end_op();
+      return -1;
+    }
   }
 
   if((f = filealloc()) == 0 || (fd = fdalloc(f)) < 0){
@@ -482,5 +499,34 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void){
+  char path[MAXPATH];
+  char target[MAXPATH];
+  struct inode *ip;
+  int n;
+
+  if(argstr(1, path, MAXPATH) < 0 || (n = argstr(0, target, MAXPATH)) < 0)
+    return -1;
+
+  begin_op();
+  if(namei(path) != 0){
+    end_op();
+    return -1;
+  }
+
+  ip = create(path, T_SYMLINK, 0, 0);
+  if(ip == 0){
+    end_op();
+    return -1;
+  }
+  if(writei(ip, 0, (uint64) &target, 0, MAXPATH) != MAXPATH)
+    panic("symlink: writei");
+
+  iunlockput(ip);
+  end_op();
   return 0;
 }
